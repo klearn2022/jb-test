21fa1e776f0fa992850f720ae10bb76430a9b81b
Kaja <kaja.learning@gmail.com>
2022-09-26 12:34:12 +0200

    JB alignemnt

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 // This fun will create middlewre in nodejs app
 app.use(express.urlencoded());
 
-app.get('', (req,res)=>{
+app.get('/', (req,res)=>{
   res.render('index');
 })
 
